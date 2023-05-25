c249b90869a3dfd7dabfaac0c3d234d107ff7f31
benjizhai <ben@zhai.ch>
2023-05-25 16:39:08 +0200

    init

---
+++ Wallet.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/Wallet.sol";
+
+contract WalletTest is Test {
+    Wallet public wallet;
+
+    function setUp() public {
+        wallet = new Wallet();
+        wallet.setNumber(0);
+    }
+
+    function testIncrement() public {
+        wallet.increment();
+        assertEq(wallet.number(), 1);
+    }
+
+    function testSetNumber(uint256 x) public {
+        wallet.setNumber(x);
+        assertEq(wallet.number(), x);
+    }
+}
