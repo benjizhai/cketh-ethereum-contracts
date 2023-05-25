fcc008de41ed8d76975531563cff6c7d7db8ddb0
benjizhai <ben@zhai.ch>
2023-05-25 18:56:07 +0200

    init

--- factory/src/Factory.sol
+++ factory/src/Factory.sol
@@ -2,15 +2,22 @@
 pragma solidity ^0.8.20;
 
 import "openzeppelin-contracts/access/Ownable.sol";
+import "openzeppelin-contracts/security/ReentrancyGuard.sol";
 import "openzeppelin-contracts/proxy/Clones.sol";
 import "../../wallet/src/Wallet.sol";
 
-contract Factory is Ownable {
+contract Factory is Ownable, ReentrancyGuard {
     address _walletImplementation;
     mapping (bytes32 => address) _wallets;
     bytes32[] _principalsList;
 
     event Spawned(bytes32 principal, address addr);
+    event CollectedETH(Wallet[] wallets, uint256[] amounts);
+    event CollectedERC20(address token, Wallet[] wallets, uint256[] amounts);
+    event CollectedERC721(address token, Wallet[] wallets, uint256[] tokenids);
+    event SentETH(address[] wallets, uint256[] amounts);
+    event SentERC20(address token, address[] wallets, uint256[] amounts);
+    event SentERC721(address token, address[] wallets, uint256[] tokenids);
 
     constructor() {
         _walletImplementation = address(new Wallet());
@@ -24,25 +31,52 @@ contract Factory is Ownable {
         emit Spawned(principal, addr);
     }
 
-    function collectETH(Wallet[] calldata wallets, uint256[] calldata amounts) external onlyOwner {
+    function collectETH(Wallet[] calldata wallets, uint256[] calldata amounts) external onlyOwner nonReentrant {
         require(wallets.length == amounts.length, "Factory: input length mismatch");
         for (uint256 i = 0; i < wallets.length; i++) {
             wallets[i].withdraw(amounts[i]);
         }
+        emit CollectedETH(wallets, amounts);
     }
 
-    function collectERC20(address token, Wallet[] calldata wallets, uint256[] calldata amounts) external onlyOwner {
+    function collectERC20(address token, Wallet[] calldata wallets, uint256[] calldata amounts) external onlyOwner nonReentrant {
         require(wallets.length == amounts.length, "Factory: input length mismatch");
         for (uint256 i = 0; i < wallets.length; i++) {
             wallets[i].withdrawERC20(token, amounts[i]);
         }
+        emit CollectedERC20(token, wallets, amounts);
     }
 
-    function collectERC721(address token, Wallet[] calldata wallets, uint256[] calldata tokenids) external onlyOwner {
+    function collectERC721(address token, Wallet[] calldata wallets, uint256[] calldata tokenids) external onlyOwner nonReentrant {
         require(wallets.length == tokenids.length, "Factory: input length mismatch");
         for (uint256 i = 0; i < wallets.length; i++) {
             wallets[i].withdrawERC721(token, tokenids[i]);
         }
+        emit CollectedERC721(token, wallets, tokenids);
+    }
+
+    function sendETH(address[] calldata wallets, uint256[] calldata amounts) external onlyOwner nonReentrant {
+        require(wallets.length == amounts.length, "Factory: input length mismatch");
+        for (uint256 i = 0; i < wallets.length; i++) {
+            payable(wallets[i]).transfer(amounts[i]);
+        }
+        emit SentETH(wallets, amounts);
+    }
+
+    function sendERC20(address token, address[] calldata wallets, uint256[] calldata amounts) external onlyOwner nonReentrant {
+        require(wallets.length == amounts.length, "Factory: input length mismatch");
+        for (uint256 i = 0; i < wallets.length; i++) {
+            IERC20(token).transfer(wallets[i], amounts[i]);
+        }
+        emit SentERC20(token, wallets, amounts);
+    }
+
+    function sendERC721(address token, address[] calldata wallets, uint256[] calldata tokenids) external onlyOwner nonReentrant {
+        require(wallets.length == tokenids.length, "Factory: input length mismatch");
+        for (uint256 i = 0; i < wallets.length; i++) {
+            IERC721(token).safeTransferFrom(address(this), wallets[i], tokenids[i]);
+        }
+        emit SentERC721(token, wallets, tokenids);
     }
 
     function computeAddress(bytes32 principal) external view returns (address addr) {
